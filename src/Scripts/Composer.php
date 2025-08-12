b10a8ae68367087ec7b39e384d97c335bf074f32
tmw-mm <toby@mirrorandmountain.com>
2025-08-12 16:25:11 +0100

    Fix feature namespace missing top folder

--- src/Scripts/Composer.php
+++ src/Scripts/Composer.php
@@ -280,7 +280,8 @@ class Composer
             exit(1);
         }
 
-        $namespace = self::$themeConfig['features_namespace'] ?? 'App\\Features\\' . $featureName;
+        $namespacePrefix = self::$themeConfig['features_namespace'] ?? 'App\\Features\\';
+        $namespace = $namespacePrefix . '\\' . $featureName;
 
         // These scripts are assumed to be in the same directory as this Composer.php file
         $scriptPathWindows = realpath(__DIR__ . DIRECTORY_SEPARATOR . 'create-feature.bat');
