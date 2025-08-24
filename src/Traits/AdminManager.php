4fb86833ed1fe87cf48b6005cc44dd8ea6650ede
tmw-mm <toby@mirrorandmountain.com>
2025-08-24 22:16:13 +0000

    Add styles theme settings tab

--- src/Traits/AdminManager.php
+++ src/Traits/AdminManager.php
@@ -37,6 +37,7 @@ trait AdminManager
 
         $this->options_map = [
             'blocks'        => $themeSettingsPageID,
+            'styles'        => $themeSettingsPageID,
             'miscellaneous' => $themeSettingsPageID
         ];
     }
