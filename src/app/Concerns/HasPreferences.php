ebe0ca8983baf35f536fd734d5f1c8c542bf302f
tmw-mm <toby@mirrorandmountain.com>
2026-04-16 06:59:17 +0100

    Update default blocks path

--- src/app/Concerns/HasPreferences.php
+++ src/app/Concerns/HasPreferences.php
@@ -3,9 +3,7 @@
 namespace MM\Meros\App\Concerns;
 
 use Illuminate\Support\Str;
-
 use MM\Meros\App\Theme;
-use MM\Meros\App\Package;
 
 trait HasPreferences {
     /**
@@ -17,14 +15,13 @@ trait HasPreferences {
         'assets_path'                      => 'resources/assets/build', // No leading or trailing slashes
         'assets_are_enabled_by_default'    => true, // Whether to enable discovered assets by default.
         'assets_are_switchable_by_default' => true, // Whether to allow enabling/disabling assets in WP Admin by default.
-        'blocks_path'                      => 'blocks/build', // No leading or trailing slashes
+        'blocks_path'                      => 'resources/blocks/build', // No leading or trailing slashes
         'blocks_are_enabled_by_default'    => true, // Whether to enable discovered blocks by default.
         'blocks_are_switchable_by_default' => true, // Whether to allow enabling/disabling blocks in WP Admin by default.
         'components_path'                  => 'src/app/View/Components', // No leading or trailing slashes
         'views_path'                       => 'resources/views', // No leading or trailing slashes
         'routes_path'                      => 'routes', // No leading or trailing slashes
         'migrations_path'                  => 'database/migrations', // No leading or trailing slashes
-        'settings_path'                    => 'config/settings.php', // No leading or trailing slashes
     ];
 
     /**
@@ -32,7 +29,7 @@ trait HasPreferences {
      *
      * @var array
      */
-    private array $preferences = [];
+    protected array $preferences = [];
 
     /**
       * Initialises preferences based on the type of the item (theme or package).
@@ -46,22 +43,14 @@ trait HasPreferences {
                 'allow_installers_in_wp_admin' => true, // Whether to allow installation of packages and themes via WP Admin.
             ];
         }
-
-        else if ($this instanceof Package) {
-            $this->defaultPreferences = [
-                ...$this->defaultPreferences,
-                'is_enabled_by_default' => true, // Whether the package is enabled by default.
-                'is_switchable'         => true, // Whether the package can be switched on or off in WP Admin.
-            ];
-        }
     }
 
     /**
      * Sets preference using the given key and value.
      * Values must match the type of the default preference value to be set.
      *
-     * @param  string $key
-     * @param  mixed  $value
+     * @param string $key
+     * @param mixed  $value
      *
      * @return void
      */
@@ -70,6 +59,10 @@ trait HasPreferences {
         $typeMatches = gettype($value) === gettype($this->defaultPreferences[$key]);
 
         if ($exists && $typeMatches) {
+            if (Str::endsWith($key, '_path')) {
+                $value = trim($value, '/'); // Remove leading and trailing slashes for path preferences.
+            }
+            
             $this->preferences[$key] = $value;
         }
     }
@@ -77,8 +70,8 @@ trait HasPreferences {
     /**
      * Returns the value of a specific preference.
      *
-     * @param  string $key
-     * @param  bool   $fullPath Whether to return the full path (including the default path) or just the custom value set by the developer (only relavant for path preferences).
+     * @param string $key
+     * @param bool   $fullPath Whether to return the full path (including the default path) or just the custom value set by the developer (only relavant for path preferences).
      *
      * @return mixed
      */
