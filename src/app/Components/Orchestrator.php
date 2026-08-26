6ba4423b5a6854182cb66449acb38001d73fa6b7
tmw-mm <toby@mirrorandmountain.com>
2026-08-26 18:03:43 +0100

    Register additional field types

--- src/app/Components/Orchestrator.php
+++ src/app/Components/Orchestrator.php
@@ -2,30 +2,41 @@
 
 namespace MM\Meros\App\Components;
 
-use MM\Meros\App\Components\Fields\Text;
-use MM\Meros\App\Components\Fields\Number;
 use MM\Meros\App\Components\Fields\Checkbox;
+use MM\Meros\App\Components\Fields\Checkboxes;
+use MM\Meros\App\Components\Fields\Date;
 use MM\Meros\App\Components\Fields\Email;
 use MM\Meros\App\Components\Fields\Hidden;
-use MM\Meros\App\Components\Fields\Repeater;
-use MM\Meros\App\Components\Fields\Checkboxes;
+use MM\Meros\App\Components\Fields\Number;
+use MM\Meros\App\Components\Fields\Password;
 use MM\Meros\App\Components\Fields\Radio;
+use MM\Meros\App\Components\Fields\Repeater;
 use MM\Meros\App\Components\Fields\Select;
+use MM\Meros\App\Components\Fields\Tel;
+use MM\Meros\App\Components\Fields\Text;
+use MM\Meros\App\Components\Fields\Time;
+use MM\Meros\App\Components\Fields\Url;
+
 use MM\Meros\App\Components\FieldGroups\SimpleContact;
 
 use MM\Meros\Contracts\Orchestrators\ComponentsOrchestrator;
 
 class Orchestrator extends ComponentsOrchestrator {
     private array $fields = [
-        'text'       => Text::class,
-        'number'     => Number::class,
         'checkbox'   => Checkbox::class,
-        'email'      => Email::class,
-        'repeater'   => Repeater::class,
-        'hidden'     => Hidden::class,
         'checkboxes' => Checkboxes::class,
+        'date'       => Date::class,
+        'email'      => Email::class,
+        'hidden'     => Hidden::class,
+        'number'     => Number::class,
+        'password'   => Password::class,
         'radio'      => Radio::class,
-        'select'     => Select::class
+        'repeater'   => Repeater::class,
+        'select'     => Select::class,
+        'tel'        => Tel::class,
+        'text'       => Text::class,
+        'time'       => Time::class,
+        'url'        => Url::class,
     ];
 
     private array $fieldGroups = [
